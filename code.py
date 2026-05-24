e4062ebb79f4f5601fa0baee663894a0b78d608e
Shaina Mae Perong <perongshainamae260@gmail.com>
2026-05-24 19:31:46 +0800

    Add contact and delete contact functions

--- code.py
+++ code.py
@@ -53,3 +53,68 @@ def refresh_table(data=None):
             table.insert("", "end",
                          values=(i, c[0], c[1], c[2]),
                          tags=("oddrow",))
+
+def add_contact():
+    try:
+        name = name_entry.get().strip()
+        phone = phone_entry.get().strip()
+        address = address_entry.get().strip()
+
+
+        if not name or not phone or not address:
+            messagebox.showwarning("Error", "Please fill all fields!")
+            return
+
+
+        contacts = load_contacts()
+        contacts.append([name, phone, address])
+        save_contacts(contacts)
+
+
+        clear_fields()
+        refresh_table()
+        status_label.config(text="Added successfully.")
+
+
+    except Exception as e:
+        messagebox.showerror("Add Error", str(e))
+
+
+def delete_contact():
+    try:
+        selected = table.focus()
+        if not selected:
+            messagebox.showwarning("Warning", "Select a contact first!")
+            return
+
+
+        values = table.item(selected)["values"]
+        if not values or values[1] == "No contact found":
+            return
+
+
+        index = int(values[0]) - 1
+        contacts = load_contacts()
+
+
+        confirm = messagebox.askyesno(
+            "Confirm Delete",
+            "Are you sure you want to delete this contact?"
+        )
+
+
+        if not confirm:
+            return
+
+
+        if 0 <= index < len(contacts):
+            contacts.pop(index)
+
+
+        save_contacts(contacts)
+        refresh_table()
+        status_label.config(text="Deleted successfully.")
+
+
+    except Exception as e:
+        messagebox.showerror("Delete Error", str(e))
